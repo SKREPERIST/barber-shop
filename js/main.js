e613f1ed88a2c10939695240b7c6b43a438f3bba
SKREPERIST <skreperist666@gmail.com>
2021-03-27 12:40:17 +0200

    final

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
 $(function () {
-
-
-  $('.slider').slick();
-
+  $(".slider").slick();
 
   new WOW().init();
 
-
-  $('.new__slider').slick({
+  $(".new__slider").slick({
     infinite: true,
     slidesToShow: 5,
     slidesToScroll: 1,
     variableWidth: true,
     dots: true,
     arrows: false,
-    centerMode: true
+    centerMode: true,
   });
 
+  let bur = document.querySelector(".burger");
+  bur.onclick = () => {
+    bur.classList.toggle("active");
+    document.querySelector(".header__menu").classList.toggle("active");
+  };
 });
